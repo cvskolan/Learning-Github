cfa10f23326a0867d8e3698bcb34dc32a61ceac9
Pelle Mårtenson <pelle.martenson@me.com>
2022-04-29 10:17:11 +0200

    Initial Commit

---
+++ Larning Github/Larning_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Larning_GithubApp.swift
+//  Larning Github
+//
+//  Created by ab86008 on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Larning_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
